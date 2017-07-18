de50c370ce8cca7d56d6f34f775eb9f969a7e115
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-07-18 23:19:16 +0300

    colorguess almost done

--- colorguess/script.js
+++ colorguess/script.js
@@ -1,13 +1,79 @@
-var colors [
-            "rgb(255, 0, 0)",
-            "rgb(255, 255, 0)",
-            "rgb(255, 0, 255)",
-            "rgb(0, 0, 0)",
-            "rgb(120, 0, 0)",
-            "rgb(255, 10, 0)"
-          ];
-var squares = documen.querySelectorAll(".square");
+var squares = document.querySelectorAll(".square");
+var length = 6;
+var random = [];
+var colors = [];
+var pickedColor = Math.floor(Math.random()*length);
+var message = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var reset = document.querySelector("#reset");
+var easy = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
 
-for (var i = 0; i < squares.length; i++) {
-  squares[i].style.backgroundColor = colors[i];
+easy.addEventListener("click", function() {
+   easy.classList.add("selected");
+   hard.classList.remove("selected");
+   length = 3;
+   squares[3].classList.add("hide");
+   squares[4].classList.add("hide");
+   squares[5].classList.add("hide");
+   resetGame();
+});
+
+hard.addEventListener("click", function() {
+   hard.classList.add("selected");
+   easy.classList.remove("selected");
+   length = 6;
+   squares[3].classList.remove("hide");
+   squares[4].classList.remove("hide");
+   squares[5].classList.remove("hide");
+   resetGame();
+});
+
+reset.addEventListener("click", function() {
+   resetGame();
+});
+
+window.onload = function() {
+   randomSquares();
+}
+
+function randomSquares() {
+   for (var i=0; i<length; i++) {
+      for(var u=0; u<3; u++) {
+         random[u] = Math.floor(Math.random()*256);
+      }
+      colors.push ("rgb(" + random[0] + ", " + random[1] + ", " + random[2] + ")");
+      squares[i].style["background-color"] = colors[i];
+
+      squares[i].addEventListener("click", function() {
+         var clickedColor = this.style["background-color"];
+         if(clickedColor==colors[pickedColor])
+         {
+            message.textContent = "Right!";
+            changeColors(clickedColor);
+            reset.textContent = "Play Again?";
+
+         }
+         else {
+            this.style["background-color"] = "#232323";
+            message.textContent = "Wrong!";
+         }
+      });
+   }
+   colorDisplay.textContent = colors[pickedColor];
+}
+
+function changeColors(color) {
+   h1.style["background-color"] = color;
+   for (var i=0; i<length;i++) {
+      squares[i].style["background-color"] = color;
+   }
+}
+
+function resetGame() {
+   random = [];
+   colors = [];
+   pickedColor = Math.floor(Math.random()*length);
+   randomSquares();
+   h1.style["background-color"] = "steelblue";
 }
