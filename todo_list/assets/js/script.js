f495c2fc0ee8a1c86a9f4aee106ba9ad8a778638
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-07-29 14:29:49 +0300

    todo-list progress. Bootstrap removed

--- todo_list/assets/js/script.js
+++ todo_list/assets/js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    initButto
+   //  initButtons();
+
+
+      // function() {
+      //    $(this).children(".btn").addClass("remove")
+      // }
+   // );
+
+   $("ul").on ("click", ".todo", function() {
+      $(this).toggleClass("crossout");
+   });
+
+   $("input[type='text']").on("change", function() {
+      var value = $(this).val();
+      $("ul").append('<li class="todo list-group-item"><span class="remove btn">X</span><span class="item">' + " " + value + '</span></li>');
+      $(this).val("");
+   });
+
+   $("ul").on("mouseover", ".todo", function() {
+      $(this).children(".btn").removeClass("remove")
+   });
+
+   $("ul").on("mouseleave", ".todo", function() {
+      $(this).children(".btn").addClass("remove")
+   });
+
+   $("ul").on("click", ".btn", function() {
+      $(this).parent("li").remove();
+   });
 });
